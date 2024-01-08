762f682e6a108346e69c793751a0cdb30f49533f
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-01-08 14:18:56 +0100

    Packages

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='font-semibold text-9xl flex justify-center items-center'>Hejsan svejsan</h1>
+    </>
+  )
+}
+
+export default App
