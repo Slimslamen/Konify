a77eb076668654a81d7392d85fa283875e7b76d2
martamalm <144331091+martamalm@users.noreply.github.com>
2024-01-09 10:43:21 +0100

    lagt in färg

--- src/Firstpagefolder/FirstPage.jsx
+++ src/Firstpagefolder/FirstPage.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import NavBar from "./NavBar"
 import MainSection from "./MainSection"
 
 
 function FirstPage (){
     return(     
         <>
-
+            <NavBar />
             <MainSection />
             
         </>
