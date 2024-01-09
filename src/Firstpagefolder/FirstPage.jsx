ee91de8fea871c36a689f3fced4b3357b4bd4f27
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-01-09 10:11:26 +0000

    footer done and responsive

--- src/Firstpagefolder/FirstPage.jsx
+++ src/Firstpagefolder/FirstPage.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Footer from "./Footer"
 import MainSection from "./MainSection"
 
 
@@ -7,6 +8,7 @@ function FirstPage (){
         <>
 
             <MainSection />
+            <Footer/>
             
         </>
     )
