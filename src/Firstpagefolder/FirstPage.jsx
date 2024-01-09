1068d5d178d6ae40d9a536435cd4a51fbbd0e976
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-01-09 09:53:37 +0100

    icon install

--- src/Firstpagefolder/FirstPage.jsx
+++ src/Firstpagefolder/FirstPage.jsx
@@ -1,13 +1,13 @@
-
+import React from "react"
+import MainSection from "./MainSection"
 
 
 function FirstPage (){
-
-
-    return(
+    return(     
         <>
-        
-        
+
+            <MainSection />
+            
         </>
     )
 }
--- src/Firstpagefolder/MainSection.jsx
+++ src/Firstpagefolder/MainSection.jsx
@@ -1,12 +1,21 @@
-
+import React from "react"
+import { Icon } from '@iconify/react';
 
 function MainSection (){
 
     return(
-        <>
-
-        
-        </>
+        <main className="flex justify-center items-center">
+            <section>
+                <img src="src/images/heroimg.png" alt="Girl with orange shirt touching her hair" />
+            </section>
+                <button>SALES</button>
+                <button>NEWS</button>
+            <section>
+                <img src="src/images/Sales.jpg" alt="Girl with orange sweater and 2 oranges infront of her eyes" />
+                <img src="src/images/News.jpg" alt="Guy with a dark T-shirt laying down on a carpet" />
+            </section>
+            <Icon icon="mingcute:up-fill" />
+        </main>
     )
 }
 
