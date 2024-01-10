8645bced01be54ff465900a1d718fe66c4e091fc
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-01-10 10:28:08 +0100

    scroll and hover

---
+++ src/Scroll.jsx
@@ -0,0 +1,12 @@
+import ScrollToTop from 'react-scroll-to-top'
+
+function Scroll() {
+
+    return(
+        <>
+           <ScrollToTop smooth className='animation font-bold  bg-purple-300 bg-opacity-40 p-2 rounded-full flex justify-center items-center right-4'/>
+        </>
+    )
+}
+
+export default Scroll
