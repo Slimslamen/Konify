e627484f96a6ccb05efc9a2f2cdbea0b77c2fa99
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-01-11 09:16:51 +0000

    fetch API

--- src/Tshirts/Tshirts.jsx
+++ src/Tshirts/Tshirts.jsx
@@ -1,16 +1,28 @@
 import NavBar from "../Firstpagefolder/NavBar.jsx";
 import Footer from "../Firstpagefolder/Footer.jsx" ;
 import CardBtn from "./CardBtn.jsx";
+import { useState } from "react";
+const Product_URL = 'https://fakestoreapi.com/products/1';
 
 function Tshirts() {
-
+    const [product, setProduct] = useState({})
+    async function fetchProducts () {
+        const response = await fetch(Product_URL);
+        const jsonResponse = await response.json();
+        const tshirt = jsonResponse.product
+        setProduct(tshirt)
+    }
     return(
-        <>
+    
+                <main>
+<button onClick={fetchProducts}>see title</button>
+<h1>{product.image}</h1>
             <NavBar />
                 <CardBtn />
             <Footer />
-        </>
+            </main>
+
     )
 }
+export default Tshirts;
 
-export default Tshirts
