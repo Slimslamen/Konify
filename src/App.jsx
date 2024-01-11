6e3f89208e742d06276b5c5d00a0eea8ae270e6c
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-01-11 10:08:51 +0100

    Buttons for cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import FirstPage from './Firstpagefolder/FirstPage'
-
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <FirstPage />
+        <FirstPage />
     </>
   )
 }
