50445ffde4b4f0ea67db988fe49a6add66c1b0fa
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-01-16 12:03:20 +0000

    style image

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,14 +4,15 @@ import CardBtn from "./CardBtn.jsx";
 const Product = ({ id, imageUrl, title }) => {
     console.log("Product:", { id, imageUrl, title });
   
-  return (<div className="tshirt-item p-4 w-1/1 md:w-1/2 lg:w-1/4">
+  return (
+  <div className="tshirt-item p-4 w-1/1 md:w-1/2 lg:w-1/4">
     <div className="min-h-[250px]">
     <img
     
       key={id}
       src={imageUrl}
       alt={title}
-      className="max-w-full mb-2"
+      className="max-w-full mb-2 h-[300px] w-[225px]"
     />
      {/* Putting the CardBtn component below the image */}
      </div>
