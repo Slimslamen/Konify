7c9e86e38c1c9a83df2b3ab2f34aeb137ef66566
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-01-19 09:22:47 +0000

    product detail page

--- src/Pages/Productdetails.jsx
+++ src/Pages/Productdetails.jsx
@@ -1,6 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { ProductContext } from "../Components/ProductContext.jsx";
+import AddtocartButton from "../Components/AddtocartButton.jsx";
+import ScrollToTop from "react-scroll-to-top";
+import Favorites from "../Components/Favorites.jsx";
+import SelectSize from "../Productpage/SelectSize.jsx"
 
 const Productdetails = () => {
   const { productId } = useParams();
@@ -13,20 +17,33 @@ const Productdetails = () => {
   console.log(selectedProduct.image);
 
   return (
-    <div>
-      <img
-        key={selectedProduct.id}
-        src={`/${selectedProduct.image}`}
-        alt={selectedProduct.title}
-        className="max-w-full mb-2 h-[300px] w-[225px] shadow-xl"
-      />
-      <h2>{selectedProduct.title}</h2>
-      <p>{selectedProduct.description}</p>
-      <p>
+    <div className="md:flex grid grid-col-1 flex-wrap mb-4 ">
+    <div className="flex items-center justify-center mb-2 w-full md:justify-end md:w-1/2 ">
+        <img
+          key={selectedProduct.id}
+          src={`/${selectedProduct.image}`}
+          alt={selectedProduct.title}
+          className="max-w-full md:m-4 w-[350px] h-[380px] md:w-[425px] md:h-[500px] shadow-xl"
+        />
+      </div>
+      <div className="md:w-1/2 w-full mb-4 flex flex-col md:justify-center">
+      <div className="flex justify-around pb-2 border-b-2">
+      <h2 className="md:text-3xl">{selectedProduct.title}</h2>
+      <p className="md:text-xl">
         Price: €{selectedProduct.price}{" "}
         <span className="text-red-500">{selectedProduct.sale}</span>
       </p>
+      </div>
+      <p className="mx-4 md:my-6 md:text-lg">{selectedProduct.description}</p>
+      <SelectSize/>
+   <div className="flex justify-around py-2 md:my-4  border-b-2">
+      <Favorites/> <AddtocartButton />
     </div>
+    </div>
+   
+    <ScrollToTop  />
+    </div>
+   
   );
 };
 
