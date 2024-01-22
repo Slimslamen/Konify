cfdb61a5c4120c67cb2aa4008c45e6d05728db9f
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-01-22 07:26:29 +0000

    fix size

--- src/Productpage/Size.jsx
+++ src/Productpage/Size.jsx
@@ -10,8 +10,8 @@ const Size = ({ size, onClick, isSelected }) => {
     <div
       onClick={handleClick}
       className={`${
-        isSelected ? "bg-slate-200 border-slate-400" : " border-none"
-      } bg-slate-50 cursor-pointer hover:bg-slate-100  p-3 w-7 h-7 m-2 flex items-center text-center justify-center`}
+        isSelected ? "bg-slate-300 border-slate-400" : " border-none"
+      } bg-slate-50 cursor-pointer hover:bg-slate-300  p-3 w-7 h-7 m-2 flex items-center text-center justify-center`}
     >
       <span>{size}</span>
     </div>
