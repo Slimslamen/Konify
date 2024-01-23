930efcc5d64b9a3978237d83a54f50499b0502d3
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-01-23 20:01:58 +0100

    checkout updated

--- src/Cart/productCart.jsx
+++ src/Cart/productCart.jsx
@@ -2,10 +2,6 @@ import { useContext, useState, useEffect } from "react";
 import { ProductContext } from "../Components/ProductContext";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
-import { useContext, useState, useEffect } from "react";
-import { ProductContext } from "../Components/ProductContext";
-import { Icon } from "@iconify/react";
-import { Link } from "react-router-dom";
 
 const ProductCart = ({ cart }) => {
   const [scores, setScores] = useState(cart.map(() => 1));
@@ -54,7 +50,7 @@ const ProductCart = ({ cart }) => {
           Shopping cart
         </h1>
       </div>
-      <div className=" md:w-100 grid grid-cols-4 justify-between md:mt-8 md:mb-0 md:m-20">
+      <div className=" md:w-100 grid grid-cols-4 md:grid-cols-6 justify-between md:mt-8 md:mb-0 md:m-20">
         <div className=" text-left ml-2">
           <p>Items</p>
         </div>
@@ -71,14 +67,14 @@ const ProductCart = ({ cart }) => {
       </div>
 
       <div className="md:w-100 border-t border-gray-300 md:mt-8 md:mb-0 md:m-20"></div>
-      <div className="md:w-100 border border-gray-300 rounded md:p-4 md:mt-4 md:m-20">
+      <div className="md:w-100 border border-gray-300 rounded md:p-4 md:mt-4 md:m-20 relative">
       {cart.map((product, idx) => (
-  <div key={product.id} className="items-start grid grid-cols-4 gap-4 justify-between mb-4">
+  <div key={product.id} className="items-start grid grid-cols-4 md:grid-cols-6 gap-4 justify-between mb-4">
     <div className="flex items-center">
       <img
         src={`/${product.image}`}
         alt={product.title}
-        className="max-w-full h-[100px] w-[100px] shadow-xl mb-5"
+        className="max-w-full h-[100px] w-[100px] shadow-xl mb-5 rounded-sm"
       />
       <div className="flex flex-col ml-2">
         <p className="w-40">{product.title}</p>
@@ -124,12 +120,23 @@ const ProductCart = ({ cart }) => {
 
 
 
-        <div className="flex items-end justify-end">
-          <div>
-            <p>Total price: {totalPrice}</p>
+        <div className="flex flex-col justify-around it rounded-l-md bg-purple-300 w-64 h-72 md:top-[-18px] md:right-0 md:absolute text-white mx-auto mb-10">
+          <img src="Assets/checkoutpic.png" alt="pile of clothes" className="w-[7em] absolute bottom-60 left-14 md:top-[-6.5em] md:left-[-3em] block md:hidden"/>
+          <h2 className="mx-3 font-bold text-center">SUMMARY</h2>
+          <div className="px-5 flex justify-between">
+            <p>Subtotal </p>
+            <p>€{totalPrice}</p>
+          </div>
+          <div className="px-5 flex justify-between">
+            <p>Taxes </p>
+            <p>-</p>
+          </div>
+          <div className="px-5 flex justify-between mb-5">
+            <p>Happy </p>
+            <p>Always.</p>
           </div>
           <Link to="/Checkout">
-            <button className="bg-purple-300 shadow-lg py-2 px-4 rounded md:ml-20 bg-gradient-to-r hover:from-purple-300 hover:to-purple-400 text-white">
+            <button className="bg-purple-300 shadow-lg py-2 w-40 rounded md:ml-20 bg-gradient-to-r hover:from-purple-300 hover:to-purple-400 text-white absolute right-[6.5em] bottom-6 md:right-10 md:bottom-[-10px]">
               Go to checkout
             </button>
           </Link>
