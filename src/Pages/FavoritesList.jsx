2d3a9d500c475c5d89c649715d7b2fa78ed9f2af
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-01-29 18:05:22 +0000

    favorites

--- src/Pages/FavoritesList.jsx
+++ src/Pages/FavoritesList.jsx
@@ -4,7 +4,7 @@ function FavoritesList({ favorites }) {
   if (favorites.length === 0) {
     return (
         <div className="flex flex-col items-center justify-center m-10">
-        <h1 className="text-5xl mb-4">Your Favorites</h1>
+        <h1 className="text-5xl mb-4">Favorites</h1>
         <h2 className="text-3xl mb-4 text-gray-600">Oops, it&#39;s really quiet in here...</h2>
         <p className="text-lg mb-8 text-gray-500 ">
           Explore our products and add them to your favorites to have a look at them later.
